eafa527690f57b83c34f159f1c2db72317c5c37d
tgstone <thstone@ucsd.edu>
2025-09-02 18:10:25 -0700

    used sphinx to create documentation tree and created a docs.yml file to automate github pages creation

---
+++ docs/build/html/_sources/index.rst.txt
@@ -0,0 +1,20 @@
+.. sio_math_workshop documentation master file, created by
+   sphinx-quickstart on Tue Sep  2 17:57:41 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+sio_math_workshop documentation
+===============================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Workshop Materials 
+   
+   notebooks/example_notebook
+   about
+
---
+++ docs/source/conf.py
@@ -0,0 +1,49 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../..'))
+
+
+project = 'sio_math_workshop'
+copyright = '2025, Tommy Stone'
+author = 'Tommy Stone'
+release = '1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_parser", # Markdwon
+    "nbsphinx",    # Jupyter Notebooks
+    "sphinx_rtd_theme", 
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+#html_theme = 'alabaster'
+#html_static_path = ['_static']
+## Use Read the Docs theme
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+
+# Allow Markdown
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
